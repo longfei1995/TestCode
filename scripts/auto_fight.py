df1d7961a670593dccfaa9ed43976b5362313ef7
Hu Yunhao <hnu_huyunhao1995@163.com>
2025-02-27 14:23:52 +0800

    打包成一个类

--- scripts/auto_fight.py
+++ scripts/auto_fight.py
@@ -2,14 +2,22 @@ import key_api
 import time
 
 
-
-
+class TLBBHelper:
+    def __init__(self):
+        self.game_helper = key_api.GameHelper()
+        
 
 def printMousePosition():
     for i in range(10):
         mouse_position = key_api.pyautogui.position()
         print(mouse_position)
         time.sleep(1)
+    
+    
+    
+    
+    
+    
     
 
 
